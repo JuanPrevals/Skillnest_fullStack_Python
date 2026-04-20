60502599288ca773c1c40638b15a16441ec8ffd8
Juan Prevals <34948337+JuanPrevals@users.noreply.github.com>
2026-04-20 10:18:35 -0400

    Update bucle_for_basico1.py

--- python/flask/fundamentos/fundamentos/bucles_for_basicos1_(core)_prevals_2026-04-14/bucle_for_basico1.py
+++ python/flask/fundamentos/fundamentos/bucles_for_basicos1_(core)_prevals_2026-04-14/bucle_for_basico1.py
@@ -70,3 +70,36 @@ def contador_dinamico(inicio, fin, salto):
 
 # Ejemplo: si inicio = 3, fin = 10, y salto = 2
 # Se imprimiría: 4, 6, 8, 10
+
+
+#Menu de selecion de ejercicios
+def menu():
+    print("Selecciona un ejercicio:")
+    print("1. Generador de niveles")
+    print("2. Potenciadores de energía")
+    print("3. Trampa de emojis")
+    print("4. Suma colosal")
+    print("5. Retroceso temporal")
+    print("6. Contador dinámico")
+
+    opcion = int(input("Ingresa el número del ejercicio que deseas ejecutar: "))
+
+    if opcion == 1:
+        generador_de_niveles()
+    elif opcion == 2:
+        potenciadores_de_energia()
+    elif opcion == 3:
+        trampa_de_emojis()
+    elif opcion == 4:
+        suma_colosal()
+    elif opcion == 5:
+        retroceso_temporal()
+    elif opcion == 6:
+        inicio = int(input("Ingresa el valor de inicio: "))
+        fin = int(input("Ingresa el valor de fin: "))
+        salto = int(input("Ingresa el valor de salto: "))
+        contador_dinamico(inicio, fin, salto)
+    else:
+        print("Opción no válida. Por favor, selecciona un número del 1 al 6.")
+if __name__ == "__main__":    menu()
+    
